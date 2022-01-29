c3589dd40e1a3c28c73045f47246f51939695802
João Paulo Sousa <jpaulo.vmsousa@yahoo.com.br>
2022-01-29 17:54:09 -0300

    Commit base do projeto utilizando Spring

---
+++ src/main/java/br/org/jprojects/desafiodesignpatternsspring/DesafioDesignPatternsSpringApplication.java
@@ -0,0 +1,13 @@
+package br.org.jprojects.desafiodesignpatternsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDesignPatternsSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioDesignPatternsSpringApplication.class, args);
+    }
+
+}
